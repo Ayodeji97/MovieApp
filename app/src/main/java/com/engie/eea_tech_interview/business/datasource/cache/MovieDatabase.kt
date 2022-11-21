59944fd5dd7755cea528e5c455ce55fef36caa28
Ayodeji97 <danielayodeji97@gmail.com>
2022-11-21 06:35:35 +0100

    added room database

--- app/src/main/java/com/engie/eea_tech_interview/business/datasource/cache/MovieDatabase.kt
+++ app/src/main/java/com/engie/eea_tech_interview/business/datasource/cache/MovieDatabase.kt
@@ -1,4 +1,16 @@
 package com.engie.eea_tech_interview.business.datasource.cache
 
-class MovieDatabase  {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.engie.eea_tech_interview.business.datasource.cache.dao.MovieDao
+import com.engie.eea_tech_interview.business.datasource.cache.model.MovieEntity
+
+@Database(
+    entities = [MovieEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class MovieDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
 }
--- app/src/main/java/com/engie/eea_tech_interview/business/datasource/cache/cachesource/GetMoviesCacheSource.kt
+++ app/src/main/java/com/engie/eea_tech_interview/business/datasource/cache/cachesource/GetMoviesCacheSource.kt
@@ -1,4 +1,16 @@
 package com.engie.eea_tech_interview.business.datasource.cache.cachesource
 
-interface GetMoviesCacheSource {
+import androidx.room.Dao
+import com.engie.eea_tech_interview.business.datasource.cache.dao.MovieDao
+import com.engie.eea_tech_interview.business.datasource.cache.model.MovieEntity
+import kotlinx.coroutines.flow.Flow
+
+class GetMoviesCacheSource(
+    private val movieDao: MovieDao
+) {
+    suspend fun saveMovies(movies: List<MovieEntity>) =
+        movieDao.insert(movies)
+
+    suspend fun getMovies(): Flow<List<MovieEntity>> =
+        movieDao.getMovies()
 }
